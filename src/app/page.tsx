a008172ad7f81ed8ec33804a7c3dfdb2506f9f46
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2026-01-20 23:14:46 -0300

    :sparkles: feat: Atualizaação de elementos e informações no home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,17 @@ export default function HomePage() {
   return (
     <>
       <main className="min-h-screen bg-teal-500">
-          <header className="flex flex-row justify-between items-center p-6 ">
-            <Link href="/" target="_self" title="Página Principal"><h1 className="bg-gradient-to-r from-slate-600 via-slate-700 to-slate-800 text-transparent bg-clip-text">TecSustentável</h1></Link>
+          <header className="flex flex-row justify-between items-center p-6 mb-20 bg-amber-400 ">
+            <Link href="/" target="_self" title="Página Principal"><h1 className="text-4xl bg-gradient-to-r from-slate-600 via-slate-700 to-slate-800 text-transparent bg-clip-text">TecSustentável</h1></Link>
             
             <nav>
               <Navegacao />
             </nav>
           </header>
 
-          <section>
-
+          <section className="bg-cyan-950 rounded-md border-black outline shadow-md w-5xl text-center p-3.5">
+            <h1 className="text-8xl font-sans uppercase font-semibold text-blue-900 ">EcoTecnologia</h1>
+            <h2 className="text-9xl font-sans uppercase font-semibold text-gray-900 ">é o agora</h2>
           </section>
       </main>
     </>
